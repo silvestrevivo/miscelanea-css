d93e4650541597be61730e40c2aa96e88ce5486f
silvestrevivo <silvestrevivomillan@gmail.com>
2019-10-20 15:18:18 +0200

    Updates

--- miscelanea-css.65ebd122.js
+++ miscelanea-css.65ebd122.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{"./../img/header.jpg":[["header.cb22ee79.jpg","Y1dg"],"Y1dg"]}],"Focm":[function(require,module,exports) {
 "use strict";require("./assets/sass/style.scss");
 },{"./assets/sass/style.scss":"xBSs"}]},{},["Focm"], null)
-//# sourceMappingURL=miscelanea-css.45309e3a.js.map
+//# sourceMappingURL=miscelanea-css.65ebd122.js.map
